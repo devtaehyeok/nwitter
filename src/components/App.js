b3c9747b29b5a5211c2bd65950c3936030393057
devTaehyeok <ith13579@korea.ac.kr>
2020-11-28 12:21:33 +0900

    이미지 첨부 실습 끝

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function App() {
       {init ? 
        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       : "Initializing..."}
-      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+         <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
 }
